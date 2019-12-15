8582e1e570510b823bad703b09ab25c77fe74a79
hyun <rlaalgus132@naver.com>
2019-12-15 15:50:18 +0900

    첫번째 프래그먼트 수정   - xml 따로 그려주기

---
+++ app/src/main/java/com/example/a20191215_fragment/fragments/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.example.a20191215_fragment.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.a20191215_fragment.R
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment :Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        okBtn.setOnClickListener {
+            Toast.makeText(activity,"${nameEdt.text.toString()} / ${nicknameEdt.text.toString()}",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
